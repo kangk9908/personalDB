f7ece9deee39a99202b077def2bde5cc73c41e80
Kun Kang <55457908+kangk9908@users.noreply.github.com>
2023-04-11 23:16:08 -0400

    Update customer_demand_view.sql

--- customer_demand_view.sql
+++ customer_demand_view.sql
@@ -1,1 +1,12 @@
-
+CREATE VIEW Customer_Demand_And_Inventory AS
+SELECT 
+    t.item_id, 
+    t.item_name, 
+    SUM(oli.Quantity) AS CustomerDemand
+FROM 
+    items t
+    JOIN order_line_items oli ON t.item_id = oli.item_id
+    JOIN customer_order co ON oli.order_id = co.order_id
+GROUP BY 
+    t.item_id, 
+    t.item_name
