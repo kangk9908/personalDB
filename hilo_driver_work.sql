2e049a5327c564a1837c0bd1a679659252dab6d9
Kun Kang <55457908+kangk9908@users.noreply.github.com>
2023-04-11 23:18:03 -0400

    Update hilo_driver_work.sql

--- hilo_driver_work.sql
+++ hilo_driver_work.sql
@@ -1,1 +1,9 @@
-
+CREATE VIEW Assigned_Work_To_HiLo AS
+SELECT 
+    wo.work_order_id, 
+    wo.product_id, 
+    hd.driver_name
+FROM 
+    work_order wo
+    JOIN trip_plans tp ON wo.trip_id = tp.trip_id
+    JOIN hilo_drivers hd ON hd.driver_id = tp.driver_id
